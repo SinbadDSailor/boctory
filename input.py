7578ebb48f1b6b046543f72c5914ce2218980697
SinbadDSailor <107209695+SinbadDSailor@users.noreply.github.com>
2023-03-26 21:22:43 +0200

    Update

--- input.py
+++ input.py
@@ -27,7 +27,7 @@ def exec_comm(conn, create_table_sql, in_vars):
     except Error as e:
         logger.error(e)
 
-def input_main():
+def input_bots():
     file_path = "storage/bots.db"
 
     print("Enter data value by value")
@@ -62,9 +62,60 @@ def input_main():
     if conn is not None:
         exec_comm(conn, ind_input_comm, input_vars)
         conn.commit()
-        print(colors.BGREEN + "Successfully")
+        print(colors.BGREEN + "Successfully executed input!" + colors.END)
         conn.close()
     else:
         logger.error("Error while trying to individual input")
+
+def input_content():
+    file_path = "storage/bots.db"
+
+    print("Enter data value by value")
+    print("If you don't want to input data, just skip input question")
+    print("---------------------------------------------------------")
+    platform = input("Enter platform marker: ")
+    heading = input("Enter heading: ")
+    paragraph = input("Enter paragraph: ")
+    image = input("Enter image path: ")
+    
+
+    input_vars_checker = (platform, heading, paragraph, image)
+    input_vars = ()
+    for val in input_vars_checker:
+        if val == "":
+            val = None
+            input_vars = input_vars + (val,)
+        else:
+            input_vars = input_vars + (val,)
+
+    ind_input_comm = """INSERT INTO content 
+    (id, 
+    platform, 
+    heading,
+    paragraph,
+    image) VALUES (null, ?, ?, ?, ?)"""
+
+    conn = create_connection(file_path)
+
+    if conn is not None:
+        exec_comm(conn, ind_input_comm, input_vars)
+        conn.commit()
+        print(colors.BGREEN + "Successfully executed input!" + colors.END)
+        conn.close()
+    else:
+        logger.error("Error while trying to individual input")
+
+def input_main():
+    print("Tables: ")
+    print(colors.BRED + "[1] bots")
+    print(colors.BRED + "[2] content" + colors.END)
+    table_option = input("Enter your option: ")
+
+    if int(table_option) == 1:
+        input_bots()
+    elif int(table_option) == 2:
+        input_content()
+    else:
+        print(colors.BRED + "Invalid option!" + colors.END)
 
 input_main()
--- term_output.py
+++ term_output.py
@@ -21,12 +21,22 @@ def create_connection(db_file):
 
     return conn
 
-def output_print_term(): # Print output to terminal function---------->
+def output_term_main(): # Print output to terminal function---------->
     file_path = "storage/bots.db"
+
+    print_term_menu()
+    term_option = input("Enter your option: ")
+
+    if int(term_option) == 1:
+        tab_val = "bots"
+    elif int(term_option) == 2:
+        tab_val = "content"
+    else:
+        print(colors.BRED + "Invalid option!" + colors.END)
 
     conn = create_connection(file_path)
 
-    print_out_comm = """SELECT * FROM bots;"""
+    print_out_comm = f"""SELECT * FROM {tab_val};"""
 
     if conn is not None:
         table = pd.read_sql_query(print_out_comm, conn)
@@ -36,6 +46,9 @@ def output_print_term(): # Print output to terminal function---------->
         logger.error("Error while trying to output to terminal")
                          # Print output to terminal function---------/>
 
+def print_term_menu():
+    print("Choose table you want to print out: ")
+    print(colors.BRED + "[1] bots")
+    print(colors.BRED + "[2] content" + colors.END)
 
-
-output_print_term()
+output_term_main()
